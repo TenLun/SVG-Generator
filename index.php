18a655dfa0669cceabb24d781885f14ed36c2cc5
Tenlun <87482603+TenLun@users.noreply.github.com>
2023-09-22 19:51:13 +0800

    先找个东西垫着

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        div{
+            border-style:solid;
+            border-width:1px;
+        }
+        h1{
+            text-align:right;
+        }
+    </style>
+    <meta name="content-type"; charset="UTF-8">
+<body>
+<div>
+    <h1><?php echo $_GET["name"];?>
+</div>
+</body>
