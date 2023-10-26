b0400f699ecbe9cb9f9c0b73e1cf2512b54e8f2e
Tenlun <87482603+TenLun@users.noreply.github.com>
2023-10-26 21:46:37 +0800

    Add language

--- script/js.js
+++ script/js.js
@@ -1,52 +1,75 @@
-function getParams () {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    repo = urlParams.get('name');
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+input = urlParams.get('name');
+options = urlParams.get('opt') || null;
+
+//画表
+function RenderChart (datas,label,colors) {
+    const ctx = document.getElementById('myChart');
+    const data = {
+        labels: label,
+        datasets: [{
+            label: '环形图实例',
+            data: datas,
+            backgroundColor: colors,
+            hoverOffset: 4
+        }]
+    };
+    const config = {
+        type: 'doughnut',
+        data: data,
+        options: {
+            responsive: true, // 设置图表为响应式，根据屏幕窗口变化而变化
+            maintainAspectRatio: false,// 保持图表原有比例
+        }
+    };
+    const myChart = new Chart(ctx, config);
 }
 
-getParams()
-
-languages = []
-datas = []
-
-async function fetchMovies() {
-  await fetch('https://api.github.com/repos/' + repo + '/languages')
-    .then( response => response.json() )
-    .then( data => json_data = data);
-
-  for ( key in json_data ) {
-    languages.push(key);
-    datas.push(json_data[key]);
-  }
-
-  RenderChart()
+function ObjToList(obj,pos){
+    var list = [];
+    for (i in obj) {
+        if (pos) list.push(obj[i]);
+        else list.push(i);
+    }
+    return list
 }
 
-fetchMovies()
+async function fetchUser(username) {
 
-function RenderChart () {
-  const ctx = document.getElementById('myChart');
-  const data = {
-    labels: languages,
-    datasets: [{
-      label: '环形图实例',
-      data: datas,
-      backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(0, 162, 235)'
-      ],
-      hoverOffset: 4
-    }]
-  };
-  const config = {
-    type: 'doughnut',
-    data: data,
-    options: {
-      responsive: true, // 设置图表为响应式，根据屏幕窗口变化而变化
-      maintainAspectRatio: false,// 保持图表原有比例
-    }
-  };
-  const myChart = new Chart(ctx, config);
+    var languages = {}
+    colors = []
+
+    await fetch('https://api.github.com/users/' + username + '/repos')
+        .then( response => response.json() )
+        .then( data => json_data = data);
+
+    //将获取到的数据添加
+    for ( i in json_data ) {
+
+        var repo = json_data[i]['full_name']
+
+        await fetch('https://api.github.com/repos/' + repo + '/languages')
+            .then( response => response.json() )
+            .then( data => lan_data = data);
+
+        //遍历语言
+        for ( lang in lan_data ) {
+            //不存在则赋值
+            if (!(languages[lang] += lan_data[lang])) {
+                languages[lang] = lan_data[lang];
+                colors.push(language_colors[lang]['color']);
+            }
+        }
+
+    }
+
+    datas = ObjToList(languages,1);
+    labels = ObjToList(languages,0);
+
+    RenderChart(datas,labels,colors);
 }
+fetchUser(input);
+
+
 
