a9ee089479af1fe15ee872abc58e3b057ce0c835
Tenlun <87482603+TenLun@users.noreply.github.com>
2024-02-25 15:40:10 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
+<?php echo '<?xml version="1.0" encoding="iso-8859-1"?>';?>
+
 <?php
-$str = $_GET["name"];
-$colors = array("red","green","yellow","blue");
-for ($i=0; $i<=strlen($str); $i++)
-{
-    $sel = $colors[rand(0,4)];
-    $result = substr($str, $i, 1);
-    echo "<a style='color:$sel'>$result</a>";
-}
+    $str = $_GET["name"];
+    header("Content-type: image/svg+xml");
 ?>
+
+<svg width="310" height="140" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <defs>
+        <path id="path1" d="M75,20 a1,1 0 0,0 100,0" />
+    </defs>
+    <text x="10" y="100" style="fill:red;">
+        <textPath xlink:href="#path1"><?php echo $str ?></textPath>
+    </text>
+</svg>
